641bd610a9b9762d9efac97b5c7aeac00ca7cb78
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-06-07 09:11:28 -0400

    initial commit

---
+++ Bridge Waterways- Pair Programming/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Waterways__Pair_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+            Rivers Cuyahoga = new Rivers();
+            Cuyahoga.Name = "Cuyahoga";
+            Console.WriteLine("Would you like to check the status of the {0} river?", Cuyahoga.Name);
+            string statusResponse = Console.ReadLine().ToLower();
+
+            if (statusResponse == "yes")
+            {
+                Cuyahoga.CheckStatus();
+                Console.WriteLine("Would you like to drain the river?");
+                string drainResponse = Console.ReadLine().ToLower();
+                Console.WriteLine("Would you like to clean the river?");
+                string cleanResponse = Console.ReadLine().ToLower();
+
+                if (drainResponse == "yes")
+                {
+                    Cuyahoga.Drain();
+                }
+            }
+        }
+    }
+}
---
+++ Bridge Waterways- Pair Programming/Rivers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Waterways__Pair_Programming
+{
+    class Rivers
+    {
+        //Declare fields
+       private string name;
+        private int currentDepth;
+        private int pollutionLevel;
+
+        //Declare properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int CurrentDepth
+        {
+            get { return this.currentDepth; }
+            set { this.currentDepth = value; }
+        }
+
+        public int PollutionLevel
+        {
+            get { return this.pollutionLevel; }
+            set { this.pollutionLevel = value; }
+        }
+
+        public Rivers()
+        {
+            //Default constructor
+        }
+
+        public Rivers(string name, int currentDepth, int pollutionLevel)
+        {
+            this.name = name;
+            this.currentDepth = currentDepth;
+            this.pollutionLevel = pollutionLevel;
+        }
+
+
+        public void Clean()
+        {
+            PollutionLevel = PollutionLevel - 10;
+            Console.WriteLine("The new depth is {0}.", PollutionLevel);
+        }
+
+        public void Drain()
+        {
+            CurrentDepth = CurrentDepth - 10;
+            Console.WriteLine("The new depth is {0}.", CurrentDepth);
+        }
+
+
+
+        public void CheckStatus()
+        {
+            Random r = new Random();
+
+            CurrentDepth = r.Next(0, 70);
+            PollutionLevel = r.Next(0, 20);
+
+            if (CurrentDepth >= 30)
+            {
+                Console.WriteLine("The current depth is {0}. The river is getting full.", CurrentDepth);
+
+            }
+
+            if (PollutionLevel >= 10)
+            {
+                Console.WriteLine("The Current pollution level is {0}. The river is getting dirty.", PollutionLevel);
+            }
+        }
+    }
+}
